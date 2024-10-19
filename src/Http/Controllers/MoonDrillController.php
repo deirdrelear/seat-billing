12ae133b76cd3da868f4ec1595d5c3721b9ec3d2
deirdrelear <rb.18@yandex.ru>
2024-10-19 17:13:02 +0300

    debug

--- src/Http/Controllers/MoonDrillController.php
+++ src/Http/Controllers/MoonDrillController.php
@@ -4,12 +4,18 @@ namespace Denngarr\Seat\Billing\Http\Controllers;
 
 use Seat\Web\Http\Controllers\Controller;
 use Seat\Eveapi\Models\Corporation\CorporationStructure;
+use Illuminate\Support\Facades\DB;
 
 class MoonDrillController extends Controller
 {
     public function index()
     {
-        $structures = CorporationStructure::where('type_id', 81826)->take(5)->get();
+        $structures = DB::table('corporation_structures')
+            ->select(DB::raw('COUNT(*) as count'), 'corporation_id')
+            ->where('type_id', 81826)
+            ->groupBy('corporation_id')
+            ->get();
+
         return view('billing::moondrills', compact('structures'));
     }
 }
--- src/resources/views/moondrills.blade.php
+++ src/resources/views/moondrills.blade.php
@@ -9,13 +9,34 @@
             <h3 class="card-title">Metenox Moon Drills</h3>
         </div>
         <div class="card-body">
-            <ul>
-            @forelse($structures as $structure)
-                <li>{{ $structure->name }}</li>
-            @empty
-                <li>Нет доступных структур</li>
-            @endforelse
-            </ul>
+            <table class="table table-striped" id="moondrills-table">
+                <thead>
+                    <tr>
+                        <th>Corporation ID</th>
+                        <th>Number of Structures</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @forelse($structures as $structure)
+                        <tr>
+                            <td>{{ $structure->corporation_id }}</td>
+                            <td>{{ $structure->count }}</td>
+                        </tr>
+                    @empty
+                        <tr>
+                            <td colspan="2">Нет доступных структур</td>
+                        </tr>
+                    @endforelse
+                </tbody>
+            </table>
         </div>
     </div>
 @endsection
+
+@push('javascript')
+<script>
+    $(document).ready(function() {
+        $('#moondrills-table').DataTable();
+    });
+</script>
+@endpush
