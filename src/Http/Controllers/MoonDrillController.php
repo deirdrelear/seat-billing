dc2039c019beeccde41cff5877ea0b0dcb93005c
deirdrelear <rb.18@yandex.ru>
2024-10-19 17:03:41 +0300

    debug

--- src/Http/Controllers/MoonDrillController.php
+++ src/Http/Controllers/MoonDrillController.php
@@ -4,7 +4,6 @@ namespace Denngarr\Seat\Billing\Http\Controllers;
 
 use Seat\Web\Http\Controllers\Controller;
 use Seat\Eveapi\Models\Corporation\CorporationStructure;
-use Seat\Eveapi\Models\Corporation\CorporationInfo;
 use Illuminate\Support\Facades\DB;
 
 class MoonDrillController extends Controller
@@ -12,21 +11,10 @@ class MoonDrillController extends Controller
     public function index()
     {
         try {
-            $user = auth()->user();
-            
-            $characterIds = $user->characters->pluck('character_id')->toArray();
-            
-            $corporationIds = DB::table('corporation_members')
-                ->whereIn('character_id', $characterIds)
-                ->pluck('corporation_id')
-                ->unique()
-                ->toArray();
-    
             $structures = CorporationStructure::with('corporation')
-                ->whereIn('corporation_id', $corporationIds)
                 ->where('type_id', 81826) // Metenox Moon Drill
                 ->get();
-    
+
             return view('billing::moondrills', compact('structures'));
         } catch (\Exception $e) {
             \Log::error('Error in MoonDrillController: ' . $e->getMessage());
